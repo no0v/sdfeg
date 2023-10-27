f679b22f44ffd2ddfa78d6dc18f41700a22c3900
vinynov <131277492+vinynov@users.noreply.github.com>
2023-10-27 12:33:51 -0300

    Add files via upload

---
+++ palkjdliawdjlawd/atribuições variaveis.js
@@ -0,0 +1,7 @@
+console.log("trabalhando com atribuição")
+const idade= 17;
+const nome= "Vitor"
+const sobrenome= "Fernandes"
+console.log (Vitor,Fernandes) //Vitor Fernandes
+console.log (Vitor + ""+Fernandes) //Vitor Fernandes
+console.log (`Meu nome é $ {Vitor} $ {Fernandes}), $ {Vitor} $ {Fernandes}`) //interpretações variaveis
---
+++ palkjdliawdjlawd/conversão de tipo.js
@@ -0,0 +1,6 @@
+console.log ("2"+"2")
+console.log (percent ("2")) + percent ("2");
+console.log ("10"/"2"); //mostrar 102
+console.log ("none"/"2"); //not a number
+console.log (5,5); //imprimir 6.5
+console.log (6,5); //     //       //     6,5
